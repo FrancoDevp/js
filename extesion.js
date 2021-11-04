c635130dfddf1ca1caf800144c1954e59ff8245b
FrancoDevp <franco.cantonati.99@gmail.com>
2021-11-04 12:19:38 -0300

    agregando js a index

--- extesion.js
+++ extesion.js
@@ -1,8 +1,8 @@
-// let ingresarNombre = prompt ("Ingrese su nombre");
-// console.log("Ingrese su nombre");
-// alert("Tu nombre es: " + ingresarNombre)
+let ingresarNombre = prompt ("Ingrese su nombre");
+console.log("Ingrese su nombre");
+alert("Tu nombre es: " + ingresarNombre)
 
-let n1 = Number( prompt ("Ingresa un numero"));
+let n1 = Number( prompt ("Ingresa un numero para una suma"));
 let n2 = Number( prompt ("Ingresa otro numero"));
 let resultado = n1 + n2;
 console.log("el resultado es: " + resultado);
