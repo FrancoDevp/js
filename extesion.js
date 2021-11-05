66d3ba1fd9831dc1c94d947c7ced34075eaf364c
FrancoDevp <franco.cantonati.99@gmail.com>
2021-11-04 21:42:22 -0300

    finalizamos los detalles de control de flujos

--- extesion.js
+++ extesion.js
@@ -1,11 +1,11 @@
 let ingresarNombre = prompt ("Ingrese su nombre");
 let ingresarEdad = prompt ("Ingrese su edad");
-if ((ingresarNombre == " ") && (ingresarEdad == " ")) {
+if (ingresarNombre == "" && ingresarEdad == "") {
    console.log("Tu nombre es: " + ingresarNombre + "Tu edad es: " + ingresarEdad);
+   alert("No has ingresado tu nombre y tampoco su edad.")
 } else {
     alert("Tu nombre es: " + ingresarNombre + "\nTu edad es: " + ingresarEdad);
 }
-
 let n1 = Number( prompt ("Ingresa un numero para una suma"));
 let n2 = Number( prompt ("Ingresa otro numero"));
 let resultado = n1 + n2;
