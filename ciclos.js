9ec20ab002b5e58099256d81b2bb5a3c2fd419c9
FrancoDevp <franco.cantonati.99@gmail.com>
2021-11-11 14:47:41 -0300

    hicimos un programa donde el usaurio debe adivinar un numero del 1 al 10

---
+++ ciclos.js
@@ -0,0 +1,15 @@
+// let saludo = "Hola Coder!";
+// for (let i = 0; i <= 10; i++){
+//     alert(saludo + " Este es el indice: " + i);
+// }
+
+alert("Haz entrado a un concurso de adivinar un numero, si ganas, ganaras un ¡premio!");
+let numero = prompt("Ingrese un numero del 1 al 10.")
+let numerosIngresados = "";
+while (numero != 7){
+    alert("El numero (" + numero + ")  no es el numero ganador");
+    numerosIngresados += numero + ",";
+    numero = prompt("Ingrese otro numero")
+}
+alert("Haz ganado el numero sorteado, el numero que ingresaste es el: " + numero + "\nTus intentos han sido estos: " +  numerosIngresados.substring(0, numerosIngresados.length - 1) + ".");
+
