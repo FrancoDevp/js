69bb38b770c3f993fddb7e9a864dc8229d8b98dc
FrancoDevp <franco.cantonati.99@gmail.com>
2021-11-17 14:51:10 -0300

    modificamos el nombre del archivo

--- funciones-clase-4.js
+++ funciones-clase-4.js
@@ -1,7 +1,7 @@
 let compra = Number(prompt("Ingrese el valor de su compra para calcular en 3, 6, 9, 12 cuotas."))
-let cuota = 3;
+const cuota = 3;
 while (cuota <= 12){
     console.log("en " + cuota + " cuotas de " + compra/cuota);
-   alert("en " + cuota + " cuotas de " + "$" +compra/cuota + " sin interes.");
+    alert("en " + cuota + " cuotas de " + "$" +compra/cuota + " sin interes.");
     cuota += 3;
 }
---
+++ objeto.js
@@ -0,0 +1,7 @@
+function Perro = (nombre, pelo, patas) {
+    this.nombre = nombre;
+    this.color = color;
+    this.sexo = sexo;
+}
+const Perro = new Perro ("luffy", "rubio", "macho");
+
