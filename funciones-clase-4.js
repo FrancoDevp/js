c33466d59a5444f9d090192e49d8c1674d2e1b00
FrancoDevp <franco.cantonati.99@gmail.com>
2021-11-17 22:42:42 -0300

    agregamos la variable envio, para que sume el

--- funciones-clase-4.js
+++ funciones-clase-4.js
@@ -1,7 +1,12 @@
 let compra = Number(prompt("Ingrese el valor de su compra para calcular en 3, 6, 9, 12 cuotas."))
+
 let cuota = 3;
+let envio = 200;
 while (cuota <= 12){
     console.log("en " + cuota + " cuotas de " + compra/cuota);
-    alert("en " + cuota + " cuotas de " + "$" + compra/cuota + " sin interes.");
+    alert("en " + cuota + " cuotas de " + "$" + compra/cuota +" sin interes.");
     cuota += 3;
 }
+alert("Si usted desea el envio, tiene un costo de $200")
+resultado = Number(prompt("ingrese el valor de la cuota que haya elegido."))
+alert("El pago total de su compra en cuotas mas el envio: $" + (resultado + envio))
